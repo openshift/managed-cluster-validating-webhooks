16f65c41ad88558fecd800fe98ba563b3b3b53f8
Lisa Seelye <lseelye@redhat.com>
2019-11-14 11:00:44 -0500

    SREP-2239: Add Subscription validation
    This changeset adds a validation webhook using the same pattern as in #1
    which will constrain dedicated-admins to making `Subscription` objects
    whose `.spec.sourceNamespace` is defined in a comma-separated list from
    the `SUBSCRIPTION_VALIDATION_N    AMESPACES` environment variable (By
    default it is only the `openshift-operators` namespace whic is a
    permitted `.spec.sourceNamespace` for dedicated-admins).
    
    This changeset also makes some other structural changes:
    
    * Move the environment variables relating to the webhooks out of the
    gunicorn.py and into the `Deployment` spec (so that it makes more sense)
    * Fundamental changes to the structure of the validation library so that
    it is now only looking for the basic structure of the AdmissionReview
    (does it have a `kind`? Is it `AdmissionReview`). This ripples on into
    the validator hooks as they now ought to use try/except blocks to
    account for missing elements in the incoming document.
    * Changes the
    group validator to use the above try/except, and to use sensible
    defaults for the two inputs it needs (group prefix, and admin group).
    Both can still be overridden by environment variables.
    
    To test the Subscription check, these two documents will put the code
    through its paces:
    
    Should fail:
    
    ```yaml
    apiVersion: operators.coreos.com/v1alpha1
    kind: Subscription
    metadata:
      name: should-fail
      namespace: myns
    spec:
      channel: stable
      name: should-fail
      source: should-fail
      sourceNamespace: default
    ```
    
    Should pass:
    
    ```yaml
    apiVersion: operators.coreos.com/v1alpha1
    kind: Subscription
    metadata:
      name: should-pass
      namespace: myns
    spec:
      channel: stable
      name: should-pass
      source: should-pass
      sourceNamespace: openshift-operators
    ```
    
    Signed-off-by: Lisa Seelye <lseelye@redhat.com>

--- src/webhook/request_helper/validate.py
+++ src/webhook/request_helper/validate.py
@@ -1,40 +1,11 @@
 def validate_request_structure(request_json):
   """Validate the request structure.
   """
-  valid = True
 
   doc_keys = request_json.keys()
 
-  valid = valid & ("request" in doc_keys)
-  request_keys = request_json['request'].keys()
-  valid = valid & ("kind" in request_keys)
-  valid = valid & ("resource" in request_keys)
-  valid = valid & ("operation" in request_keys)
-  valid = valid & ("object" in request_keys)
-
-  user_keys = request_json['request']['userInfo'].keys()
-  valid = valid & ("username" in user_keys)
-  valid = valid & ("groups" in user_keys  )
-  valid = valid & (type(request_json['request']['userInfo']['groups']) is list)
-  valid = valid & ("extra" in user_keys)
-
-  object_keys = request_json['request']['object'].keys()
-  valid = valid & ("metadata" in object_keys)
-  valid = valid & ("users" in object_keys)
-
-  metadata_keys = request_json['request']['object']['metadata'].keys()
-  valid = valid & ("name" in metadata_keys)
-  valid = valid & ("creationTimestamp" in metadata_keys)
-  valid = valid & ("uid" in metadata_keys)
-
-  resource_keys = request_json['request']['resource'].keys()
-  valid = valid & ("group" in resource_keys)
-  valid = valid & ("version" in resource_keys)
-  valid = valid & ("resource" in resource_keys)
-
-  kind_keys = request_json['request']['kind'].keys()
-  valid = valid & ("group" in kind_keys)
-  valid = valid & ("version" in kind_keys)
-  valid = valid & ("kind" in kind_keys)
-
-  return valid
+  if 'kind' not in doc_keys:
+    return False
+  if request_json['kind'] == "AdmissionReview":
+    return True
+  return False
---
+++ src/webhook/subscription_validation.py
@@ -0,0 +1,45 @@
+from flask import request, Blueprint
+import sys, traceback
+import json
+import os
+
+from webhook.request_helper import validate, responses
+
+bp = Blueprint("subscription-webhook", __name__)
+
+valid_source_namespaces = os.getenv("SUBSCRIPTION_VALIDATION_NAMESPACES", "openshift-operators")
+
+valid_source_namespaces = valid_source_namespaces.split(",")
+
+@bp.route('/subscription-validation', methods=('GET','POST'))
+def handle_request():
+  debug = os.getenv("DEBUG_SUBSCRIPTION_VALIDATION", "False")
+  debug = (debug == "True")
+
+  if debug:
+    print("REQUEST BODY => {}".format(request.json))
+
+  valid = True
+  try:
+    valid = validate.validate_request_structure(request.json)
+  except Exception:
+    valid = False
+
+  if not valid:
+    return responses.response_invalid()
+  
+  try:
+    body_dict = request.json['request']
+    requester_group_memberships = body_dict['userInfo']['groups']
+    if "dedicated-admins" in requester_group_memberships:
+      if body_dict['object']['spec']['sourceNamespace'] not in valid_source_namespaces:
+        return responses.response_deny(req=body_dict, msg="You cannot manage Subscriptions that target {}.".format(body_dict['object']['spec']['sourceNamespace']))
+      else:
+        return responses.response_allow(req=body_dict)
+    else:
+      return responses.response_allow(req=body_dict)
+  except Exception:
+    print("Exception:")
+    print("-"*60)
+    traceback.print_exc(file=sys.stdout)
+    return responses.response_invalid()
