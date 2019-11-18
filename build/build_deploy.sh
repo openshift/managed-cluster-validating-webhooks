c7c1fd1d673c4e564d972af19a34d93c9c1b50c7
Max Whittingham <max.whittingham@gmail.com>
2019-11-18 15:19:05 -0600

    Adding generate syncset script and started versioning the build deploy script work, adding selectorsyncset template

--- build/build_deploy.sh
+++ build/build_deploy.sh
@@ -8,12 +8,16 @@ CURRENT_DIR=$(dirname "$0")
 
 BASE_IMG="managed-cluster-validating-webhooks"
 QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
-IMG="${BASE_IMG}:latest"
+IMG="${BASE_IMG}:${VERSION_MAJOR}.${VERSION_MINOR}-${GIT_HASH}"
+VERSION_MAJOR=0
+VERSION_MINOR=1
 
 GIT_HASH=$(git rev-parse --short=7 HEAD)
 
 # build the image
 BUILD_CMD="docker build" IMG="$IMG" make docker-build
+
+GEN_SYNCSET=$(generate_syncset.py -t "${SELECTOR_SYNC_SET_TEMPLATE_DIR}" -y "${YAML_DIRECTORY}" -d "${SELECTOR_SYNC_SET_DESTINATION}" -r "${REPO_NAME}")
 
 # push the image
 skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
