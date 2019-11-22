01b6cb1dcc43b55e86fd9e838a57fe8339fc942d
Max Whittingham <max.whittingham@gmail.com>
2019-11-22 15:18:27 -0600

    Many fixes to the templates, build deploy script and moving the template location

--- build/build_deploy.sh
+++ build/build_deploy.sh
@@ -11,24 +11,23 @@ QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
 VERSION_MAJOR=0
 VERSION_MINOR=1
 SELECTOR_SYNC_SET_TEMPLATE_DIR=../templates
-YAML_DIRECTORY=../templates
+BUILD_DIRECTORY=../build
 SELECTOR_SYNC_SET_DESTINATION=../deploy/selectorsyncset.yaml
 REPO_NAME=managed-cluster-validating-webhooks
 GIT_HASH=$(git rev-parse --short=7 HEAD)
-IMG="$QUAY_IMAGE":"$IMAGETAG" 
 IMAGETAG="${VERSION_MAJOR}.${VERSION_MINOR}-${GIT_HASH}"
+IMG="$QUAY_IMAGE":"$IMAGETAG"
 
-# build the image
-#docker build -t build-base $IMG -f ../build/Dockerfile
+# build the image and the selectorsyncset
 
-python generate_syncset.py -t ${SELECTOR_SYNC_SET_TEMPLATE_DIR} -y ${YAML_DIRECTORY} -d ${SELECTOR_SYNC_SET_DESTINATION} -r ${REPO_NAME}
+python generate_syncset.py -t ${SELECTOR_SYNC_SET_TEMPLATE_DIR} -b ${BUILD_DIRECTORY} -d ${SELECTOR_SYNC_SET_DESTINATION} -r ${REPO_NAME}
 
+docker build -t $IMG -f Dockerfile ..
 # push the image
-#skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-#    "docker-daemon:${BASE_IMG}" \
-#    "docker://${QUAY_IMAGE}:latest"
-
-#skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-#    "docker-daemon:${BASE_IMG}" \
-#    "docker://${QUAY_IMAGE}:${GIT_HASH}"
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${BASE_IMG}" \
+    "docker://${QUAY_IMAGE}:latest"
+skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+    "docker-daemon:${BASE_IMG}" \
+    "docker://${QUAY_IMAGE}:${IMAGETAG}"
 
