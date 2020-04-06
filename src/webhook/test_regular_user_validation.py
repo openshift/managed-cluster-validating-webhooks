0b43674d01a56743cfbee1dceb0ede67aa66f3ee
Naveen Malik <nmalik@redhat.com>
2020-04-06 16:55:43 -0400

    Fixing where group info is pulled for 'regular user' webhook
    https://issues.redhat.com/browse/OSD-3242
    
    Original logic was not looking at the incoming user's groups, as expressed in userInfo.
    All SRE requests were still being denied because it didn't have any group info.  This fixes it.

--- src/webhook/test_regular_user_validation.py
+++ src/webhook/test_regular_user_validation.py
@@ -9,7 +9,7 @@ ADMIN_GROUPS = [
     "osd-sre-admins"
 ]
 
-def create_request(kind, username, groupname):
+def create_request(username, groupname):
     class FakeRequest(object):
         def __init__(self):
             self.json = {
@@ -17,19 +17,14 @@ def create_request(kind, username, groupname):
                     "uid": "testuser",
                     "operation": "UPDATE",
                     "userInfo": {
-                        "username": username
-                    },
-                    "oldObject": {
-                        "metadata": {
-                            "name": groupname
-                        }
+                        "username": username,
+                        "groups": [
+                            groupname
+                        ],
                     },
                     "object": {
-                        "kind": kind,
-                        "metadata": {
-                            "name": groupname
-                        }
-                    },
+                        "kind": "TestResources",
+                    }
                 }
             }
 
@@ -40,34 +35,34 @@ def create_request(kind, username, groupname):
 
 class TestRegularUserValidation(unittest.TestCase):
     def test_is_request_allowed_unauthenticated(self):
-        self.assertFalse(is_request_allowed("system:unauthenticated", "", ADMIN_GROUPS))
+        self.assertFalse(is_request_allowed("system:unauthenticated", [], ADMIN_GROUPS))
 
     def test_is_request_allowed_sre(self):
-        self.assertTrue(is_request_allowed("nmalik@redhat.com", ADMIN_GROUPS[0], ADMIN_GROUPS))
+        self.assertTrue(is_request_allowed("nmalik@redhat.com", [ADMIN_GROUPS[0]], ADMIN_GROUPS))
 
     def test_is_request_allowed_kubeadmin(self):
-        self.assertTrue(is_request_allowed("kube:admin", "", ADMIN_GROUPS))
+        self.assertTrue(is_request_allowed("kube:admin", [], ADMIN_GROUPS))
 
     def test_is_request_allowed_systemadmin(self):
-        self.assertTrue(is_request_allowed("system:admin", "", ADMIN_GROUPS))
+        self.assertTrue(is_request_allowed("system:admin", [], ADMIN_GROUPS))
 
     def test_is_request_allowed_nonadmin_group(self):
-        self.assertFalse(is_request_allowed("someuser", "some-group", ADMIN_GROUPS))
+        self.assertFalse(is_request_allowed("someuser", ["some-group"], ADMIN_GROUPS))
 
     def test_handle_request_allowed_system(self):
-        request = create_request("Groups", "kube:admin", ADMIN_GROUPS[0])
+        request = create_request("kube:admin", ADMIN_GROUPS[0])
         
         response = json.loads(get_response(request, debug=False))
         self.assertTrue(response['response']['allowed'])
 
     def test_handle_request_allowed_user(self):
-        request = create_request("Groups", "some-sre-user", ADMIN_GROUPS[0])
+        request = create_request("some-sre-user", ADMIN_GROUPS[0])
         
         response = json.loads(get_response(request, debug=False))
         self.assertTrue(response['response']['allowed'])
 
     def test_handle_request_denied(self):
-        request = create_request("Groups", "someuser", "")
+        request = create_request("someuser", "")
         
         response = json.loads(get_response(request, debug=False))
         self.assertFalse(response['response']['allowed'])
