67fc976973d9fb65e61304c249e171683a1cd871
Eric Fried <openstack@fried.cc>
2020-04-08 01:53:02 -0500

    DRY test_regular_user_validation.py
    Makes use of subclassing to reduce the repetition in
    test_regular_user_validation.py.

--- src/webhook/test_regular_user_validation.py
+++ src/webhook/test_regular_user_validation.py
@@ -32,131 +32,84 @@ def create_request(username, groups):
     return FakeRequest()
 
 
-class TestRegularUserValidation_Unauthenticated(unittest.TestCase):
+class RegularUserValidationBase(object):
+    """Mixin providing test logic.
+
+    To use:
+    - Create a class inheriting from both this and unittest.TestCase.
+    - Set the `username` and `expect` variables.
+      . `username` is the username in the request
+      . `expect` is a boolean indicating whether we expect the request to be
+        allowed (True) or denied (False).
+    - Override methods that behave unusually.
+    """
+    def runtests(self, testGroups, expect_override=None):
+        exp = expect_override if expect_override is not None else self.expect
+        self.assertEqual(
+            exp,
+            is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
+
+        request = create_request(self.username, testGroups)
+
+        response = json.loads(get_response(request, debug=False))
+        self.assertEqual(exp, response['response']['allowed'])
+
+    def test_noGroup(self):
+        self.runtests([])
+
+    def test_sreGroup(self):
+        self.runtests([ADMIN_GROUPS[0]])
+
+    def test_nonSreGroup(self):
+        self.runtests(["something-else"])
+
+    def test_manyGroups(self):
+        self.runtests(["something-else", "other-thing"])
+
+    def test_manyGroups_sreGroup(self):
+        self.runtests(["something-else", ADMIN_GROUPS[0]])
+
+
+class TestRegularUserValidation_Unauthenticated(unittest.TestCase,
+                                                RegularUserValidationBase):
     username = "system:unauthenticated"
-
-    def runtests(self, testGroups):
-        self.assertFalse(is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
-
-        request = create_request(self.username, testGroups)
-        
-        response = json.loads(get_response(request, debug=False))
-        self.assertFalse(response['response']['allowed'])
-
-    def test_noGroup(self):
-        self.runtests([])
-
-    def test_sreGroup(self):
-        self.runtests([ADMIN_GROUPS[0]])
-
-    def test_nonSreGroup(self):
-        self.runtests(["something-else"])
-
-    def test_manyGroups(self):
-        self.runtests(["something-else", "other-thing"])
-
-    def test_manyGroups_sreGroup(self):
-        self.runtests(["something-else", ADMIN_GROUPS[0]])
+    expect = False
 
 
-class TestRegularUserValidation_kubeadmin(unittest.TestCase):
+class TestRegularUserValidation_kubeadmin(unittest.TestCase,
+                                          RegularUserValidationBase):
     username = "kube:admin"
-
-    def runtests(self, testGroups):
-        self.assertTrue(is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
-
-        request = create_request(self.username, testGroups)
-        
-        response = json.loads(get_response(request, debug=False))
-        self.assertTrue(response['response']['allowed'])
-
-    def test_noGroup(self):
-        self.runtests([])
-
-    def test_sreGroup(self):
-        self.runtests([ADMIN_GROUPS[0]])
-
-    def test_nonSreGroup(self):
-        self.runtests(["something-else"])
-
-    def test_manyGroups(self):
-        self.runtests(["something-else", "other-thing"])
-
-    def test_manyGroups_sreGroup(self):
-        self.runtests(["something-else", ADMIN_GROUPS[0]])
+    expect = True
 
 
-class TestRegularUserValidation_systemadmin(unittest.TestCase):
+class TestRegularUserValidation_systemadmin(unittest.TestCase,
+                                            RegularUserValidationBase):
     username = "system:admin"
-
-    def runtests(self, testGroups):
-        self.assertTrue(is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
-
-        request = create_request(self.username, testGroups)
-        
-        response = json.loads(get_response(request, debug=False))
-        self.assertTrue(response['response']['allowed'])
-
-    def test_noGroup(self):
-        self.runtests([])
-
-    def test_sreGroup(self):
-        self.runtests([ADMIN_GROUPS[0]])
-
-    def test_nonSreGroup(self):
-        self.runtests(["something-else"])
-
-    def test_manyGroups(self):
-        self.runtests(["something-else", "other-thing"])
-
-    def test_manyGroups_sreGroup(self):
-        self.runtests(["something-else", ADMIN_GROUPS[0]])
+    expect = True
 
 
-class TestRegularUserValidation_sreUser(unittest.TestCase):
+class TestRegularUserValidation_sreUser(unittest.TestCase,
+                                        RegularUserValidationBase):
     username = "nmalik@redhat.com"
-    sreGroup = ADMIN_GROUPS[0]
+    expect = True
 
-    def runtests(self, testGroups):
+    def runtests(self, testGroups, expect_override=None):
         # for these tests, add the SRE group to every request
-        testGroups.append(self.sreGroup)
-        self.assertTrue(is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
+        testGroups.append(ADMIN_GROUPS[0])
+        super(TestRegularUserValidation_sreUser, self).runtests(
+            testGroups, expect_override=expect_override)
 
-        request = create_request(self.username, testGroups)
-        response = json.loads(get_response(request, debug=False))
-        self.assertTrue(response['response']['allowed'])
 
-    def test_noGroup(self):
-        self.runtests([])
+class TestRegularUserValidation_nonSreGroup(unittest.TestCase,
+                                            RegularUserValidationBase):
+    username = "someOtherUser"
+    expect = False
 
     def test_sreGroup(self):
-        self.runtests([ADMIN_GROUPS[0]])
-
-    def test_nonSreGroup(self):
-        self.runtests(["something-else"])
-
-    def test_manyGroups(self):
-        self.runtests(["something-else", "other-thing"])
+        # This one ALLOWs because we're passing the SRE group
+        self.runtests([ADMIN_GROUPS[0]], expect_override=True)
 
     def test_manyGroups_sreGroup(self):
-        self.runtests(["something-else", ADMIN_GROUPS[0]])
-
-
-class TestRegularUserValidation_nonSreGroup(unittest.TestCase):
-    username = "someOtherUser"
-
-    def runtests(self, testGroups):
-        self.assertFalse(is_request_allowed(self.username, testGroups, ADMIN_GROUPS))
-
-        request = create_request(self.username, testGroups)
-        response = json.loads(get_response(request, debug=False))
-        self.assertFalse(response['response']['allowed'])
-
-    def test_noGroup(self):
-        self.runtests([])
-
-    def test_nonSreGroup(self):
-        self.runtests(["something-else"])
-
-    def test_manyGroups(self):
-        self.runtests(["something-else", "other-thing"])
+        # This one ALLOWs because we're passing the SRE group
+        self.runtests(["something-else", ADMIN_GROUPS[0]],
+                      expect_override=True)
