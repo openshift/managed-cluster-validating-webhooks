ba784c8e0124e11701b82c17c804d4b0c6782f28
Candace Sheremeta <candace@redhat.com>
2020-05-05 17:43:08 -0400

    Add more test cases to fully cover all possibilities for ALLOW/DENY

--- src/webhook/test_group_validation.py
+++ src/webhook/test_group_validation.py
@@ -54,16 +54,26 @@ class TestGroupValidation(unittest.TestCase):
         "system:admin",
     )
 
-    PRIVILEGED_GROUPS = (
+    PRIVILEGED_GROUPS_1 = (
+        # need 2 privileged groups groupings - priv groups 1 can edit/delete
+        # priv groups 2, but priv groups 2 cannot edit priv groups 1
         # These are tuples so they're immutable, forcing the test case to
         # duplicate in order to change them. Otherwise we have potential
         # collisions among test cases.
         ("osd-sre-admins"),
         ("osd-sre-cluster-admins"),
-        ("osd-sre-test-group"),
+    )
+
+    PRIVILEGED_GROUPS_2 = (
+        # need 2 privileged groups groupings - priv groups 1 can edit/delete
+        # priv groups 2, but priv groups 2 cannot edit priv groups 1
+        # These are tuples so they're immutable, forcing the test case to
+        # duplicate in order to change them. Otherwise we have potential
+        # collisions among test cases.
         ("dedicated-admins"),
         ("cluster-admins"),
         ("layered-cs-sre-admins"),
+        ("osd-sre-test-group"),
     )
 
     NON_PRIVILEGED_USERS = (
@@ -114,38 +124,53 @@ class TestGroupValidation(unittest.TestCase):
         # regardless of whether memberGroups or editGroup is privileged
         # or non-privileged
         for user in self.PRIVILEGED_USERS:
-            for group in self.PRIVILEGED_GROUPS + self.NON_PRIVILEGED_GROUPS:
-                self.runtest(user, self.PRIVILEGED_GROUPS, group, True)
+            for group in self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS:
+                self.runtest(user, self.PRIVILEGED_GROUPS_1, group, True)
+                self.runtest(user, self.PRIVILEGED_GROUPS_2, group, True)
                 self.runtest(user, self.NON_PRIVILEGED_GROUPS, group, True)
-                self.rundeletetest(user, self.PRIVILEGED_GROUPS, group, True)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_1, group, True)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_2, group, True)
                 self.rundeletetest(user, self.NON_PRIVILEGED_GROUPS, group, True)
 
     def test_priv_group(self):
-        # If the user is in one of the PRIVILEGED_GROUPS, always ALLOW,
+        # If the user is in one of the PRIVILEGED_GROUPS_1, always ALLOW,
         # regardless of whether editGroup is privileged or non-privileged,
         # or if user is also a member of non-privileged groups
         for user in self.NON_PRIVILEGED_USERS:
-            for group in self.PRIVILEGED_GROUPS + self.NON_PRIVILEGED_GROUPS:
-                self.runtest(user, self.PRIVILEGED_GROUPS, group, True)
-                self.runtest(user, (self.PRIVILEGED_GROUPS + self.NON_PRIVILEGED_GROUPS), group, True)
-                self.rundeletetest(user, self.PRIVILEGED_GROUPS, group, True)
-                self.rundeletetest(user, (self.PRIVILEGED_GROUPS + self.NON_PRIVILEGED_GROUPS), group, True)
+            for group in self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS:
+                self.runtest(user, self.PRIVILEGED_GROUPS_1, group, True)
+                self.runtest(user, (self.PRIVILEGED_GROUPS_1 + self.NON_PRIVILEGED_GROUPS), group, True)
+                self.runtest(user, (self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2), group, True)
+                self.runtest(user, (self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS), group, True)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_1, group, True)
+                self.rundeletetest(user, (self.PRIVILEGED_GROUPS_1 + self.NON_PRIVILEGED_GROUPS), group, True)
+                self.rundeletetest(user, (self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2), group, True)
+                self.rundeletetest(user, (self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS), group, True)
 
     def test_non_priv_group(self):
-        # If the user is in NON_PRIVILEGED_GROUPS, ALLOW if editGroup is also
-        # non-privileged
+        # If the user is in PRIVILEGED_GROUP_2 and/or NON_PRIVILEGED_GROUPS, 
+        # ALLOW if editGroup is also non-privileged
         for user in self.NON_PRIVILEGED_USERS:
             for group in self.NON_PRIVILEGED_GROUPS:
                 self.runtest(user, self.NON_PRIVILEGED_GROUPS, group, True)
+                self.runtest(user, self.PRIVILEGED_GROUPS_2, group, True)
+                self.runtest(user, self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS, group, True)
                 self.rundeletetest(user, self.NON_PRIVILEGED_GROUPS, group, True)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_2, group, True)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_2 + self.NON_PRIVILEGED_GROUPS, group, True)
 
     def test_deny(self):
-        # In order to get DENY, user in non-privileged groups must be trying to
-        # edit/delete a privileged group
+        # In order to get DENY, user in PRIVILEGED_GROUPS_2 and/or 
+        # NON_PRIVILEGED_GROUPS must be trying to edit/delete a group in
+        # PRIVILEGED_GROUPS_1 or PRIVILEGED_GROUPS_2
         for user in self.NON_PRIVILEGED_USERS:
-            for group in self.PRIVILEGED_GROUPS:
+            for group in self.PRIVILEGED_GROUPS_1 + self.PRIVILEGED_GROUPS_2:
                 self.runtest(user, self.NON_PRIVILEGED_GROUPS, group, False)
+                self.runtest(user, self.PRIVILEGED_GROUPS_2, group, False)
+                self.runtest(user, self.NON_PRIVILEGED_GROUPS + self.PRIVILEGED_GROUPS_2, group, False)
                 self.rundeletetest(user, self.NON_PRIVILEGED_GROUPS, group, False)
+                self.rundeletetest(user, self.PRIVILEGED_GROUPS_2, group, False)
+                self.rundeletetest(user, self.NON_PRIVILEGED_GROUPS + self.PRIVILEGED_GROUPS_2, group, False)
 
     def test_invalid(self):
         # Validate the exception path
