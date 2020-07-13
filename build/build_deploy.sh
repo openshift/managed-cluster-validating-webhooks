4bd3435f071df14069066f5650dc46508fef5e3f
Lisa Seelye <18159+lisa@users.noreply.github.com>
2020-07-13 14:25:21 -0400

    OSD-4110: Remove IMAGE="$IMAGE" in build script
    This is now causing builds to be problematic.

--- build/build_deploy.sh
+++ build/build_deploy.sh
@@ -5,4 +5,4 @@
 set -exv
 
 # build the image, the selectorsyncset, and push the image
-make -C $(dirname $0)/../ IMAGETAG="$IMAGETAG" syncset build-base skopeo-push
+make -C $(dirname $0)/../ syncset build-base skopeo-push
