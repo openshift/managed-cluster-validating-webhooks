4f8b94fe7ac0c9d29af43729a22524248eacca69
Candace Sheremeta <candace@redhat.com>
2020-10-23 14:18:23 -0400

    Update error message for operations on default identity - OSD-5379

--- pkg/webhooks/identity/identity.go
+++ pkg/webhooks/identity/identity.go
@@ -121,7 +121,7 @@ func (s *IdentityWebhook) authorized(request admissionctl.Request) admissionctl.
 			}
 		}
 		log.Info("Denying access", "request", request.AdmissionRequest)
-		ret = admissionctl.Denied("Prevented from accessing Red Hat managed identity provider. Please manage your identity provider through https://cloud.redhat.com/openshift")
+		ret = admissionctl.Denied("Prevented from modifying Red Hat's managed Identity. You may create/modify any Identity objects, except for 'OpenShift_SRE'.")
 		ret.UID = request.AdmissionRequest.UID
 		return ret
 	}
