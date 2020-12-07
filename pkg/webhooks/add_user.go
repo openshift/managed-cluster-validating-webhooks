5d80b891428143798b8cc5bf1c29b25acafe1c05
Ari Lima <alima@redhat.com>
2020-12-07 12:34:34 -0800

    remove regular user validation

--- pkg/webhooks/add_user.go
+++ pkg/webhooks/add_user.go
@@ -1,9 +1,5 @@
 package webhooks
 
-import (
-	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/user"
-)
-
 func init() {
-	Register(user.WebhookName, func() Webhook { return user.NewWebhook() })
+	// Register(user.WebhookName, func() Webhook { return user.NewWebhook() })
 }
