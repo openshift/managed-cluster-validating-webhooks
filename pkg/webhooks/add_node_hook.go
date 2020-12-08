5c33cd3d77eb7bf4c13897e6c85e44fe6a9c1a6a
Ari Lima <alima@redhat.com>
2020-12-08 10:43:02 -0800

    change register

---
+++ pkg/webhooks/add_node_hook.go
@@ -0,0 +1,9 @@
+package webhooks
+
+import (
+	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/node"
+)
+
+func init() {
+	Register(node.WebhookName, func() Webhook { return node.NewWebhook() })
+}
--- pkg/webhooks/add_nodelabels_hook.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-import (
-	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/nodelabels"
-)
-
-func init() {
-	Register(nodelabels.WebhookName, func() Webhook { return nodelabels.NewWebhook() })
-}
