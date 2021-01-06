97f2005dc74cecfd935c9ab3f6187e83f7e32063
Ari Lima <alima@redhat.com>
2021-01-06 12:19:15 -0800

    node

--- pkg/webhooks/node/node.go
+++ pkg/webhooks/node/node.go
@@ -133,13 +133,13 @@ func (s *LabelsWebhook) authorized(request admissionctl.Request) admissionctl.Re
 		return ret
 	}
 
-	log.Info(fmt.Sprintf("new:\n%v", node.Labels))
-	log.Info(fmt.Sprintf("old:\n%v", oldNode.Labels))
-
 	// Check that the current user is a dedicated admin
 	for _, userGroup := range request.UserInfo.Groups {
 		log.Info(request.UserInfo.Username)
 		log.Info(fmt.Sprintf("user info groups: %v", userGroup))
+		log.Info(fmt.Sprintf("new:\n%v", node.Labels))
+		log.Info(fmt.Sprintf("old:\n%v", oldNode.Labels))
+
 		if contains(adminGroups, userGroup) {
 			// Only edit worker nodes
 			if _, ok := oldNode.Labels[workerLabel]; !ok {
