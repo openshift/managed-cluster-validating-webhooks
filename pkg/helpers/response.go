4e62c3c6ef7db1fce591cfa8656f15aa65700b18
Lisa Seelye <18159+lisa@users.noreply.github.com>
2021-06-02 14:18:38 -0400

    Update pkg/helpers/response.go with PR feedback
    Co-authored-by: Christoph Blecker <admin@toph.ca>

--- pkg/helpers/response.go
+++ pkg/helpers/response.go
@@ -16,10 +16,10 @@ var log = logf.Log.WithName("response_helper")
 func SendResponse(w io.Writer, resp admissionctl.Response) {
 	encoder := json.NewEncoder(w)
 	responseAdmissionReview := admissionapi.AdmissionReview{
+		APIVersion: admissionapi.SchemeGroupVersion.String(),
+		Kind: "AdmissionReview",
 		Response: &resp.AdmissionResponse,
 	}
-	responseAdmissionReview.APIVersion = "admission.k8s.io/v1"
-	responseAdmissionReview.Kind = "AdmissionReview"
 	err := encoder.Encode(responseAdmissionReview)
 	// TODO (lisa): handle this in a non-recursive way (why would the second one succeed)?
 	if err != nil {
