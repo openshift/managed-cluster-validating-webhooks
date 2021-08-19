8cdc3a463cbf974fe83d803b555c4c17182a84e6
John Roche <jroche@redhat.com>
2021-08-19 13:28:11 +1000

    Remove group webhook

--- pkg/webhooks/add_group_hook.go
+++
@@ -1,7 +0,0 @@
-package webhooks
-
-import "github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/group"
-
-func init() {
-	Register(group.WebhookName, func() Webhook { return group.NewWebhook() })
-}
