b4c235c62e7c2e17db9a0dc33cdab1dce9d63c36
John Roche <jroche@redhat.com>
2021-08-19 13:20:48 +1000

    Remove user webhook

--- pkg/webhooks/add_user.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-import (
-	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/user"
-)
-
-func init() {
-	Register(user.WebhookName, func() Webhook { return user.NewWebhook() })
-}
