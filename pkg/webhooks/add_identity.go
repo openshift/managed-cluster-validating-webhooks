80967f1eb47cf753959fb5b6694c4be1bbacfb39
John Roche <jroche@redhat.com>
2021-08-19 13:20:17 +1000

    Remove identity webhook

--- pkg/webhooks/add_identity.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-import (
-	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/identity"
-)
-
-func init() {
-	Register(identity.WebhookName, func() Webhook { return identity.NewWebhook() })
-}
