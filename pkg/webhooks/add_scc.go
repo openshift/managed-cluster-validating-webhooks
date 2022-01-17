dc85ce5dd713d55cdcfb262c8711e702e3c65804
Tereza Gabrielova <tgabriel@redhat.com>
2022-01-17 13:06:58 +0100

    Revert "add validation webhook for scc"

--- pkg/webhooks/add_scc.go
+++
@@ -1,9 +0,0 @@
-package webhooks
-
-import (
-	"github.com/openshift/managed-cluster-validating-webhooks/pkg/webhooks/scc"
-)
-
-func init() {
-	Register(scc.WebhookName, func() Webhook { return scc.NewWebhook() })
-}
