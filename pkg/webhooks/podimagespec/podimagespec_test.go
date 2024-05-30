0221ee2b3ddfb98c39b006f64fa2479764440c3c
Lisa Rashidi-Ranjbar <lranjbar@redhat.com>
2024-05-30 16:01:10 -0700

    add unit tests for pod contains regex match

--- pkg/webhooks/podimagespec/podimagespec_test.go
+++ pkg/webhooks/podimagespec/podimagespec_test.go
@@ -5,18 +5,58 @@ import (
 	"reflect"
 	"testing"
 
-	configv1 "github.com/openshift/api/imageregistry/v1"
+	registryv1 "github.com/openshift/api/imageregistry/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-type OutputImageSpecRegex struct {
+type outputImageSpecRegex struct {
 	matched   bool
 	namespace string
 	image     string
 	tag       string
+}
+
+func newMockRegistry(obs ...client.Object) (client.Client, error) {
+	s := runtime.NewScheme()
+	if err := registryv1.Install(s); err != nil {
+		return nil, err
+	}
+
+	return fake.NewClientBuilder().WithScheme(s).WithObjects(obs...).Build(), nil
+}
+
+func TestCheckImageRegistryStatus(t *testing.T) {
+	tests := []struct {
+		name     string
+		config   *registryv1.Config
+		expected bool
+	}{
+		{
+			name: "test",
+			config: &registryv1.Config{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "notfound",
+				},
+				Spec:   registryv1.ImageRegistrySpec{},
+				Status: registryv1.ImageRegistryStatus{},
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		s := NewWebhook()
+		s.kubeClient, _ = newMockRegistry(test.config)
+		actual, _ := s.checkImageRegistryStatus(context.Background())
+		if actual != test.expected {
+			t.Error("failed")
+		}
+	}
+
 }
 
 func TestCheckContainerImageSpecByRegex(t *testing.T) {
@@ -24,12 +64,12 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 	tests := []struct {
 		name      string
 		imagespec string
-		expected  OutputImageSpecRegex
+		expected  outputImageSpecRegex
 	}{
 		{
 			name:      "test uninteresting short imagespec",
 			imagespec: "ubuntu",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   false,
 				namespace: "",
 				image:     "",
@@ -39,7 +79,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 		{
 			name:      "test uninteresting short tagged imagespec",
 			imagespec: "ubuntu:latest",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   false,
 				namespace: "",
 				image:     "",
@@ -49,7 +89,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 		{
 			name:      "test uninteresting fully qualified tagged imagespec",
 			imagespec: "docker.io/library/ubuntu:latest",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   false,
 				namespace: "",
 				image:     "",
@@ -59,7 +99,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 		{
 			name:      "test uninteresting fully qualified SHA imagespec",
 			imagespec: "quay.io/openshift-release-dev/ocp-release@sha256:4dbe2a75a516a947eab036ef6a1d086f1b1610f6bd21c6ab5f95db68ec177ea2",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   false,
 				namespace: "",
 				image:     "",
@@ -70,7 +110,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 		{
 			name:      "test interesting fully qualified SHA imagespec",
 			imagespec: "image-registry.openshift-image-registry.svc:5000/openshift/cli@sha256:4dbe2a75a516a947eab036ef6a1d086f1b1610f6bd21c6ab5f95db68ec177ea2",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   false,
 				namespace: "",
 				image:     "",
@@ -80,7 +120,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 		{
 			name:      "test interesting fully qualified tagged imagespec",
 			imagespec: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest",
-			expected: OutputImageSpecRegex{
+			expected: outputImageSpecRegex{
 				matched:   true,
 				namespace: "openshift",
 				image:     "cli",
@@ -90,7 +130,7 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		actual := OutputImageSpecRegex{}
+		actual := outputImageSpecRegex{}
 		actual.matched, actual.namespace, actual.image, actual.tag = checkContainerImageSpecByRegex(test.imagespec)
 		if !reflect.DeepEqual(actual, test.expected) {
 			t.Errorf("TestCheckContainerImageSpecByRegex() %s -\n imagespec: %s \n actual: %v\n expected: %v\n", test.name, test.imagespec, actual, test.expected)
@@ -98,40 +138,137 @@ func TestCheckContainerImageSpecByRegex(t *testing.T) {
 	}
 
 }
-func NewMock(obs ...client.Object) (client.Client, error) {
-	s := runtime.NewScheme()
-	if err := configv1.Install(s); err != nil {
-		return nil, err
-	}
 
-	return fake.NewClientBuilder().WithScheme(s).WithObjects(obs...).Build(), nil
-}
-
-func TestCheckImageRegistryStatus(t *testing.T) {
+func TestPodContainsContainerRegexMatch(t *testing.T) {
 	tests := []struct {
 		name     string
-		config   *configv1.Config
+		pod      *corev1.Pod
 		expected bool
 	}{
 		{
-			name: "test",
-			config: &configv1.Config{
+			name:     "test empty pod",
+			pod:      &corev1.Pod{},
+			expected: false,
+		},
+		{
+			name: "test pod with cli image in containers",
+			pod: &corev1.Pod{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "notfound",
+					Name: "test",
 				},
-				Spec:   configv1.ImageRegistrySpec{},
-				Status: configv1.ImageRegistryStatus{},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: true,
+		},
+		{
+			name: "test pod with cli image in initcontainers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: true,
+		},
+		{
+			name: "test pod with uninteresting image in containers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Image: "ubuntu"},
+					},
+				},
+				Status: corev1.PodStatus{},
 			},
 			expected: false,
 		},
+		{
+			name: "test pod with uninteresting image in initcontainers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{
+						{Image: "ubuntu"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: false,
+		},
+		{
+			name: "test pod with cli image in initcontainers and uninteresting in containers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Image: "ubuntu"},
+					},
+					InitContainers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: true,
+		},
+		{
+			name: "test pod with cli image in containers and uninteresting in initcontainers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+					InitContainers: []corev1.Container{
+						{Image: "ubuntu"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: true,
+		},
+		{
+			name: "test pod with cli image in containers and initcontainers",
+			pod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+					InitContainers: []corev1.Container{
+						{Image: "image-registry.openshift-image-registry.svc:5000/openshift/cli:latest"},
+					},
+				},
+				Status: corev1.PodStatus{},
+			},
+			expected: true,
+		},
 	}
-
 	for _, test := range tests {
-		s := NewWebhook()
-		s.kubeClient, _ = NewMock(test.config)
-		actual, _ := s.checkImageRegistryStatus(context.Background())
+		actual := podContainsContainerRegexMatch(test.pod)
 		if actual != test.expected {
-			t.Error("failed")
+			t.Errorf("TestPodContainsContainerRegexMatch() %s -\n pod: %v \n actual: %t\n expected: %t\n", test.name, test.pod, actual, test.expected)
 		}
 	}
 
