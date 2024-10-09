4dc33c09f67d8b1bbcbaec5f4125f8a88e6ea9b4
YiqinZhang <yiqzhang@redhat.com>
2024-10-09 15:35:32 -0400

    Add OSDe2e test harness scaffolding

---
+++ test/osde2e/managed_cluster_validating_webhooks_test.go
@@ -0,0 +1,15 @@
+//go:build osde2e
+// +build osde2e
+
+package osde2etests
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Managed Cluster Validating Webhooks", Ordered, func() {
+	BeforeAll(func(ctx context.Context) {
+	})
+})
