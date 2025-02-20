ccd2254be657284bb0284d1c1b611ca59f6af258
ritmun <rmundhe@redhat.com>
2025-02-20 15:39:15 -0600

    sdcicd-1414 move osde2e/ to test/e2e

--- test/e2e/project.mk
+++ test/e2e/project.mk
@@ -9,7 +9,7 @@ REGISTRY_USER?=$(QUAY_USER)
 REGISTRY_TOKEN?=$(QUAY_TOKEN)
 
 ######################
-# Targets used by osde2e test harness
+# Targets used by e2e test harness
 ######################
 
 # create binary
@@ -18,7 +18,7 @@ e2e-harness-build: GOFLAGS_MOD=-mod=mod
 e2e-harness-build: GOENV=GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 GOFLAGS="${GOFLAGS_MOD}"
 e2e-harness-build:
 	go mod tidy
-	${GOENV} go test ./osde2e -v -c --tags=osde2e -o harness.test
+	${GOENV} go test ./test/e2e -v -c --tags=osde2e -o harness.test
 
 # TODO: Push to a known image tag and commit id
 # push harness image
@@ -28,7 +28,7 @@ HARNESS_IMAGE_TAG=$(CURRENT_COMMIT)
 
 .PHONY: e2e-image-build-push
 e2e-image-build-push:
-	${CONTAINER_ENGINE} build --pull -f osde2e/Dockerfile -t $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):$(HARNESS_IMAGE_TAG) .
+	${CONTAINER_ENGINE} build --pull -f test/e2e/Dockerfile -t $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):$(HARNESS_IMAGE_TAG) .
 	${CONTAINER_ENGINE} tag $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):$(HARNESS_IMAGE_TAG) $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):latest
 	${CONTAINER_ENGINE} push $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):$(HARNESS_IMAGE_TAG)
 	${CONTAINER_ENGINE} push $(HARNESS_IMAGE_REGISTRY)/$(HARNESS_IMAGE_REPOSITORY)/$(HARNESS_IMAGE_NAME):latest
