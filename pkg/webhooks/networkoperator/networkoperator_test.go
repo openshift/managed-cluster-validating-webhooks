70f888f70a6314f0d1c822b22990ef5a19f7cb41
Sairatnam Trinagari <strinaga@redhat.com>
2026-01-08 23:01:07 +0530

    Adding test cases to match new implementation

--- pkg/webhooks/networkoperator/networkoperator_test.go
+++ pkg/webhooks/networkoperator/networkoperator_test.go
@@ -110,7 +110,7 @@ func TestAuthorized(t *testing.T) {
 			ExpectAllowed: false,
 		},
 		{
-			Name: "privileged service account modifying migration.networkType should be denied",
+			Name: "SRE service account modifying migration.networkType should be allowed",
 			Request: admissionctl.Request{
 				AdmissionRequest: admissionv1.AdmissionRequest{
 					UserInfo: authenticationv1.UserInfo{
@@ -154,7 +154,7 @@ func TestAuthorized(t *testing.T) {
 					},
 				},
 			},
-			ExpectAllowed: false,
+			ExpectAllowed: true,
 		},
 		{
 			Name: "modifying migration.mode should be denied",
@@ -410,6 +410,50 @@ func TestAuthorized(t *testing.T) {
 							"spec": {
 								"migration": {
 									"networkType": "OVNKubernetes"
+								}
+							}
+						}`),
+					},
+				},
+			},
+			ExpectAllowed: true,
+		},
+		{
+			Name: "backplane-cluster-admin modifying migration.networkType should be allowed",
+			Request: admissionctl.Request{
+				AdmissionRequest: admissionv1.AdmissionRequest{
+					UserInfo: authenticationv1.UserInfo{
+						Username: "backplane-cluster-admin",
+					},
+					Kind: metav1.GroupVersionKind{
+						Group: "operator.openshift.io",
+						Kind:  "Network",
+					},
+					Operation: admissionv1.Update,
+					Object: runtime.RawExtension{
+						Raw: []byte(`{
+							"apiVersion": "operator.openshift.io/v1",
+							"kind": "Network",
+							"metadata": {
+								"name": "cluster"
+							},
+							"spec": {
+								"migration": {
+									"networkType": "OVNKubernetes"
+								}
+							}
+						}`),
+					},
+					OldObject: runtime.RawExtension{
+						Raw: []byte(`{
+							"apiVersion": "operator.openshift.io/v1",
+							"kind": "Network",
+							"metadata": {
+								"name": "cluster"
+							},
+							"spec": {
+								"migration": {
+									"networkType": "OpenShiftSDN"
 								}
 							}
 						}`),
